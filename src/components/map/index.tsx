45997ca703b84ec28f16fee464b3df993c8b4316
Alexandre Machado <64610360+alexandrehomero@users.noreply.github.com>
2023-06-30 11:32:36 -0300

    with-dots

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,80 +1,52 @@
-import React, { useEffect, useRef } from 'react';
-import 'ol/ol.css';
-import * as ol from 'ol';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
-import { OSM, Vector as VectorSource } from 'ol/source';
-import { Icon, Style } from 'ol/style';
-import { fromLonLat } from 'ol/proj';
-import { block } from 'million/react';
-import styled from 'styled-components';
+import { useEffect, useRef } from 'react';
 
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { fromLonLat } from 'ol/proj';
+import {Feature, Map, View} from 'ol/index.js';
+import { Point } from 'ol/geom';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import {useGeographic} from 'ol/proj';
+
+import styled from 'styled-components';
+import 'ol/ol.css';
 
 const MapBlock = () => {
-  const mapContainerRef = useRef(null);
+    useGeographic();
 
-  useEffect(() => {
-    const map = new Map({
-      target: mapContainerRef.current,
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      view: new View({
-        center: fromLonLat([-48.2772, -18.9146]), // Centered on Uberlândia
-        zoom: 6,
-      }),
-    });
+    const mapContainerRef = useRef(null);
+    const places = [[-48.2772, -18.9146],[-47.8825, -15.7939]]
+    const point = new Point(places[0])
 
-    // Uberlândia marker
-    const uberlandiaMarker = new Style({
-      image: new Icon({
-        src: 'https://openlayers.org/en/latest/examples/data/icon.png',
-      }),
-    });
+    useEffect(() => {
+        const map = new Map({
+            target: mapContainerRef.current,
+            layers: [
+            new TileLayer({
+                source: new OSM(),
+            }),
+            new VectorLayer({
+                source: new VectorSource({
+                    features: [new Feature(point)],
+                }),
+                style: {
+                    'circle-radius': 9,
+                    'circle-fill-color': 'red',
+                },
+                }),
+            ],
+            view: new View({
+            center: fromLonLat([-48.2772, -18.9146]),
+            zoom: 6,
+            }),
+        });
 
-    const uberlandiaLayer = new VectorLayer({
-      source: new VectorSource({
-        features: [
-          new ol.Feature({
-            geometry: new ol.geom.Point(fromLonLat([-48.2772, -18.9146])), // Uberlândia coordinates
-          }),
-        ],
-      }),
-      style: uberlandiaMarker,
-    });
-
-    map.addLayer(uberlandiaLayer);
-
-    // Brasília marker
-    const brasiliaMarker = new Style({
-      image: new Icon({
-        src: 'https://openlayers.org/en/latest/examples/data/icon.png',
-      }),
-    });
-
-    const brasiliaLayer = new VectorLayer({
-      source: new VectorSource({
-        features: [
-          new ol.Feature({
-            geometry: new ol.geom.Point(fromLonLat([-47.8825, -15.7939])), // Brasília coordinates
-          }),
-        ],
-      }),
-      style: brasiliaMarker,
-    });
-
-    map.addLayer(brasiliaLayer);
-
-    return () => {
-      map.setTarget(null);
-    };
-  }, []);
+        return () => {
+            map.setTarget(null);
+        };
+    }, []);
 
     return <Container ref={mapContainerRef} />;
-    }
+}
 
     const Container = styled.div`
         width: 100%;
